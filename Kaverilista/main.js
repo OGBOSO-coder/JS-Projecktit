ea93b86d8d022fb8c0c903c816eb641864e8f86c
OGBOSO-coder <Kim.tiainen@gmail.com>
2022-01-31 11:11:56 +0200

    lisäys

---
+++ Kaverilista/main.js
@@ -0,0 +1,5 @@
+let liElements = document.getElementsByClassName('list-item');
+
+for(let i=0; i < liElements.length; i++){
+    liElements[i].style.cssText = 'background-color: yellow; padding: 5px;'
+}
---
+++ part2/main.js
@@ -0,0 +1,39 @@
+let lomake = document.forms['formNewItem'];
+lomake.addEventListener('submit', uusiListaElementti)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function uusiListaElementti(event){
+
+    event.preventDefault()
+
+    let elementinNimi = document.querySelector('#main input[type="text"]').value;
+    
+    if(elementinNimi.length < 1){
+        alert('Pitää antaa nimi')
+        return;
+    }
+
+    let uusiElementti = document.createElement('li')
+    let uusiElementtiTeksti = document.createTextNode(elementinNimi);
+    uusiElementti.appendChild(uusiElementtiTeksti)
+    uusiElementti.className = 'list-item';
+
+    document.querySelector('#itemList').appendChild(uusiElementti)
+}
+
